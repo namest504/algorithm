6b71565b5b2e77703016a0147339c0e5b6cbc89c
SeungTaek Lim <namest504@gmail.com>
2026-01-28 22:58:51 +0900

    Time: 0 ms (100%), Space: 4.3 MB (73.22%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -0,0 +1,15 @@
+func longestCommonPrefix(strs []string) string {
+    prefix := strs[0]
+
+    for i :=1; i < len(strs); i++ {
+        for !strings.HasPrefix(strs[i], prefix) {
+            prefix = prefix[:len(prefix)-1]
+
+            if prefix == "" {
+                return ""
+            }
+        }
+    }
+
+    return prefix
+}
